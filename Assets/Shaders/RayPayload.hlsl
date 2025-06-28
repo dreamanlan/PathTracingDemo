a1bcda62a04474205635235e84dbcdd2144ae6ca
dreamanlan <dreaman_163@163.com>
2025-06-28 22:22:15 +0800

    Initial runnable version on mac

--- Assets/Shaders/RayPayload.hlsl
+++ Assets/Shaders/RayPayload.hlsl
@@ -1,12 +1,12 @@
 struct RayPayload
 {
     float4 color;
-    float k;                // Energy conservation constraint
-    float3 albedo;
-    float3 emission;
-    uint bounceIndexOpaque;
-    uint bounceIndexTransparent;
-    float3 bounceRayOrigin;
-    float3 bounceRayDirection;
-    uint rngState;          // Random number generator state.
+    //float k;                // Energy conservation constraint
+    //float3 albedo;
+    //float3 emission;
+    //uint bounceIndexOpaque;
+    //uint bounceIndexTransparent;
+    //float3 bounceRayOrigin;
+    //float3 bounceRayDirection;
+    //uint rngState;          // Random number generator state.
 };
